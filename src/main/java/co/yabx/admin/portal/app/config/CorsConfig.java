0f82837173a9a6be6685a50c75365aef1e10b1d0
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2019-12-24 15:31:25 +0530

    + cors fix

--- src/main/java/co/yabx/admin/portal/app/config/CorsConfig.java
+++ src/main/java/co/yabx/admin/portal/app/config/CorsConfig.java
@@ -2,6 +2,8 @@ package co.yabx.admin.portal.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -9,6 +11,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
