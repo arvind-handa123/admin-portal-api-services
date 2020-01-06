7b6a46bbd7f7aaa4ebd1ed41528d43109ba3779c
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2020-01-06 17:07:37 +0530

    + cors issue

--- src/main/java/co/yabx/admin/portal/app/controllers/LoginController.java
+++ src/main/java/co/yabx/admin/portal/app/controllers/LoginController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,7 @@ import co.yabx.admin.portal.app.kyc.service.StorageService;
 import co.yabx.admin.portal.app.service.AdminPortalService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/v1")
 public class LoginController {
 
--- src/main/java/co/yabx/admin/portal/app/controllers/RMController.java
+++ src/main/java/co/yabx/admin/portal/app/controllers/RMController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,7 @@ import co.yabx.admin.portal.app.kyc.service.StorageService;
 import co.yabx.admin.portal.app.kyc.service.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/v1")
 public class RMController {
 
