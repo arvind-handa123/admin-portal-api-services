8adde07a1f7f630c8a5b4d202f9537ded1dd6abd
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2020-01-06 20:29:17 +0530

    + cors

--- src/main/java/co/yabx/admin/portal/app/security/SecurityConfiguration.java
+++ src/main/java/co/yabx/admin/portal/app/security/SecurityConfiguration.java
@@ -71,7 +71,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://kyc.yabx.co", "http://kyc.yabx.co"));
+		configuration.setAllowedOrigins(Arrays.asList("https://kyc.yabx.co:8082", "http://kyc.yabx.co:8082"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
