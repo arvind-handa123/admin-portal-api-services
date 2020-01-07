6a7d76f9ebd7a6d58c5a6010b0b974f0b7756b08
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2020-01-07 21:40:40 +0530

    ++ corsFiletr

--- src/main/java/co/yabx/admin/portal/app/security/AuthenticationFilter.java
+++ src/main/java/co/yabx/admin/portal/app/security/AuthenticationFilter.java
@@ -37,35 +37,9 @@ public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter
 	}
 
 	@Override
-	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+	protected void successfulAuthentication(final HttpServletRequest request, final HttpServletResponse response,
 			final FilterChain chain, final Authentication authResult) throws IOException, ServletException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Allow-Headers",
-				"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Expose-Headers", "Authorization");
-		response.addHeader("Access-Control-Expose-Headers", "responseType");
-		response.addHeader("Access-Control-Expose-Headers", "observe");
-		// System.out.println("Request Method: " + request.getMethod());
-		if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
-			try {
-				chain.doFilter(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("Pre-flight");
-			response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
-			response.setHeader("Access-Control-Max-Age", "3600");
-			response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"
-					+ "Authorization, content-type," + "USERID" + "ROLE"
-					+ "access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
-			response.setStatus(HttpServletResponse.SC_OK);
-		}
 		SecurityContextHolder.getContext().setAuthentication(authResult);
+		chain.doFilter(request, response);
 	}
-
 }
