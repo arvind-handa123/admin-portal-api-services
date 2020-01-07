2576c2b2ba92e84315b80835699b4c25d903170b
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2020-01-07 21:46:56 +0530

    ++

--- src/main/java/co/yabx/admin/portal/app/security/CORSFilter.java
+++ src/main/java/co/yabx/admin/portal/app/security/CORSFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.GenericFilterBean;
@@ -18,33 +17,12 @@ public class CORSFilter extends GenericFilterBean implements Filter {
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
-		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Allow-Headers",
-				"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
+		response.setHeader("Access-Control-Allow-Methods", "*");
+		response.setHeader("Access-Control-Allow-Headers", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Expose-Headers", "Authorization");
-		response.addHeader("Access-Control-Expose-Headers", "responseType");
-		response.addHeader("Access-Control-Expose-Headers", "observe");
-		// System.out.println("Request Method: " + request.getMethod());
-		if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
-			try {
-				chain.doFilter(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("Pre-flight");
-			response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
-			response.setHeader("Access-Control-Max-Age", "3600");
-			response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"
-					+ "Authorization, content-type," + "USERID" + "ROLE"
-					+ "access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
-			response.setStatus(HttpServletResponse.SC_OK);
-		}
+		response.setHeader("Access-Control-Allow-Credentials", "false");
+		chain.doFilter(servletRequest, servletResponse);
 
 	}
 
